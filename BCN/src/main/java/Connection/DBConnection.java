493d9e4f6a392a6dbb892fdf1a6dc2c4d205e233
LourdesCabrera1505 <lourdes.90programming@gmail.com>
2024-07-05 21:53:05 -0600

    Creando paquete DBConnection , creando archivo DBConnection, creando instancia de la conección para evitar nuevas instancias

---
+++ BCN/src/main/java/Connection/DBConnection.java
@@ -0,0 +1,44 @@
+package Connection;
+import java.sql.*;
+public class DBConnection {
+
+    private static DBConnection instance;
+
+    private  Connection connection; // Creando instancia para la conección a la base de datos
+
+    // Detalles de las conección
+    private final String url = "jdbc://localhost:1433;databaseName=BCN_ParcialFinal;encrypt=true;trustServerCertificate=true"; //Asignandp la url de la base de datos
+    private final String user = "Lourdes"; // definiendo el usuario con el que se accedera a la base de datos
+    private final String password = "dream2024"; // contraseña que da el paso al usuario a la base de datos
+
+
+    // Constructor privado para evitar la creacion de instancias directas
+    private DBConnection()throws  SQLException {
+        try {
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");// Se realiza la carga del driver de sql server
+            this.connection = DriverManager.getConnection(url, user, password); // se establece la conección con la base de datos
+        }catch (ClassNotFoundException e) {
+            throw  new SQLException("El driver de sql server no ha sido encontrado", e);
+        }
+    }
+
+    // Método estático para obtener la instancia de la conección
+    public static  DBConnection getInstance() throws SQLException {
+        if (instance == null) {
+            synchronized (DBConnection.class) {
+                if (instance == null) {
+                    instance = new DBConnection();
+                }
+            }
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DBConnection(); // Si la conexión está cerrada, se crea una nueva
+        }
+        return instance;
+    }
+
+    // Método para obtener la conexión actual
+    public Connection getConnection() {
+        return connection;
+    }
+}
--- BCN/src/main/java/module-info.java
+++ BCN/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.example.bcn {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires  java.sql;
 
     opens org.example.bcn to javafx.fxml;
     exports org.example.bcn;
