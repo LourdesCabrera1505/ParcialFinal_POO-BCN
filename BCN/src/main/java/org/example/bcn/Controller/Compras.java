cf247dc003df975eb481bc6d60f58678d18c32d2
LourdesCabrera1505 <lourdes.90programming@gmail.com>
2024-07-09 17:40:05 -0600

    Actualizacion agregando paquete de conversion para spinner

--- BCN/src/main/java/org/example/bcn/Controller/Compras.java
+++ BCN/src/main/java/org/example/bcn/Controller/Compras.java
@@ -4,21 +4,36 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
-
+import
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Compras implements Initializable {
 
+    @FXML
+    private DatePicker IDFechaVenta;
+    @FXML
+    private Spinner<Double> IDPagoTotal;
+    @FXML
+    private ComboBox<String> ID_InformacionCuenta;
+    @FXML
+    private ComboBox<String> IDCompraDescripcion;
+    @FXML
+    private Button IDSendInfo;
 
     @FXML
     private  void HandleMinimizeButton (ActionEvent event) {
         Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
-    @Override
+    @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+
 }
