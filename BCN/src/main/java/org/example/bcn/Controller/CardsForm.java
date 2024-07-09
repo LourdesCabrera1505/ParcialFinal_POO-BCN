3fc41bae372e6dc8120a1075da9235fb90846e85
LourdesCabrera1505 <lourdes.90programming@gmail.com>
2024-07-09 14:52:51 -0600

    Arreglando Facilitadores

--- BCN/src/main/java/org/example/bcn/Controller/CardsForm.java
+++ BCN/src/main/java/org/example/bcn/Controller/CardsForm.java
@@ -10,7 +10,6 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.example.bcn.Connection.DBConnection;
-
 import java.net.URL;
 import java.sql.*;
 import java.time.LocalDate;
@@ -19,12 +18,10 @@ import java.util.ResourceBundle;
 
 public class CardsForm implements Initializable {
 
-    DBConnection connectionClients;
-
     @FXML
-    private ComboBox IDFacilitador;
+    private ComboBox<String> IDFacilitador;
     @FXML
-    private ComboBox IDCliente;
+    private ComboBox<String> IDCliente;
     @FXML
     private TextField IDNumber;
     @FXML
@@ -42,182 +39,128 @@ public class CardsForm implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        if (IDSendInfo != null){
+        if (IDSendInfo != null) {
             IDSendInfo.setOnAction(this::createCard);
+            try {
+                CargarCliente();
+                CargarFacilitador();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
+    @FXML
     protected void createCard(ActionEvent event) {
         try {
-            connectionClients = DBConnection.getInstance();
-            connectionClients.getConnection();
-            PreparedStatement insertSQL = connectionClients.getConnection().prepareStatement("INSERT INTO Cards (CardNumber, CardType, DateExpired, ClienteID, FacilitatorID) VALUES (?,?,?,?,?)");
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement insertSQL = connection.prepareStatement("INSERT INTO Cards (CardNumber, CardType, DateExpired, ClienteID, FacilitatorID) VALUES (?,?,?,?,?)");
             String Tarjeta = IDNumber.getText();
             String Tipo = IDTipo.getText();
-            String Cliente = TextCliente.getText();
-            String Facilitador = TextFacilitador.getText();
+            String Cliente = IDCliente.getValue();
+            String Facilitador = IDFacilitador.getValue();
             LocalDate Fecha = IDDate.getValue();
-            String FechaFormato = Fecha.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-            int IDFacilitador = ObtenerFacilitador((Connection) connectionClients, Facilitador);
-            int IDCliente = ObtenerCliente((Connection) connectionClients, Cliente);
+            int ClienteID = ObtenerCliente(connection, Cliente);
+            int FacilitadorID = ObtenerFacilitador(connection, Facilitador);
+            String FechaFormato = Fecha.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             insertSQL.setString(1, Tarjeta);
             insertSQL.setString(2, Tipo);
             insertSQL.setString(3, FechaFormato);
-            insertSQL.setInt(4, IDCliente);
-            insertSQL.setInt(5, IDFacilitador);
+            insertSQL.setInt(4, ClienteID);
+            insertSQL.setInt(5, FacilitadorID);
             insertSQL.executeUpdate();
             mostrarExito("Registro Exitoso");
             ResetForm();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            mostrarError("Error en el registro: " + e.getMessage());
         }
     }
 
     private int ObtenerFacilitador(Connection connection, String facilitador) throws SQLException {
-
-        String query = "SELECT FacilitatorID FROM FACILITATOR WHERE FacilitatorName = ?";
-        try(PreparedStatement ps = connection.prepareStatement(query)) {
+        String query = "SELECT FacilitatorID FROM Facilitator WHERE FacilitatorName = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, facilitador);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
+            if (rs.next()) {
                 return rs.getInt("FacilitatorID");
-            }else {
-                throw new SQLException("No se encontro el facilitador" + facilitador);
+            } else {
+                throw new SQLException("No se encontró el facilitador: " + facilitador);
             }
         }
     }
 
     private int ObtenerCliente(Connection connection, String cliente) throws SQLException {
-        String query = "SELECT ClienteID FROM CLIENTE WHERE NameCliente = ?";
-        try(PreparedStatement ps = connection.prepareStatement(query)) {
+        String query = "SELECT ClienteID FROM Cliente WHERE NameCliente = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, cliente);
             ResultSet rs = ps.executeQuery();
-
-            if(rs.next()) {
+            if (rs.next()) {
                 return rs.getInt("ClienteID");
-            }else {
-                throw new SQLException("No se encontro el cliente" + cliente);
+            } else {
+                throw new SQLException("No se encontró el cliente: " + cliente);
             }
         }
     }
 
     private void CargarFacilitador() throws SQLException {
-        connectionClients = DBConnection.getInstance();
-        connectionClients.getConnection();
-
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            Connection facilitador = connectionClients.getConnection();
-            st = facilitador.createStatement();
-            rs = st.executeQuery("SELECT FacilitatorName FROM Facilitator");
+        Connection connection = DBConnection.getInstance().getConnection();
+        try (Statement st = connection.createStatement(); ResultSet rs = st.executeQuery("SELECT FacilitatorName FROM Facilitator")) {
             ObservableList<String> opciones = FXCollections.observableArrayList();
-            while(rs.next()) {
+            while (rs.next()) {
                 opciones.add(rs.getString("FacilitatorName"));
             }
             IDFacilitador.setItems(opciones);
-        }catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(rs != null)
-                    rs.close();
-                if (st != null)
-                    st.close();
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 
     private void CargarCliente() throws SQLException {
-        connectionClients = DBConnection.getInstance();
-        connectionClients.getConnection();
-
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            Connection cliente = connectionClients.getConnection();
-            st = cliente.createStatement();
-            rs = st.executeQuery("SELECT NameCliente FROM Cliente");
+        Connection connection = DBConnection.getInstance().getConnection();
+        try (Statement st = connection.createStatement(); ResultSet rs = st.executeQuery("SELECT NameCliente FROM Cliente")) {
             ObservableList<String> opciones = FXCollections.observableArrayList();
-            while(rs.next()) {
+            while (rs.next()) {
                 opciones.add(rs.getString("NameCliente"));
             }
             IDCliente.setItems(opciones);
-        }catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(rs != null)
-                    rs.close();
-                if (st != null)
-                    st.close();
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    // funcion que permite limpiar los campos del formulario despues de enviar un registro
-    private void ResetForm () {
+    private void ResetForm() {
         IDNumber.clear();
         IDTipo.clear();
         IDFacilitador.setValue(null);
-        IDDate.getEditor().clear();
+        IDDate.setValue(null);  // Corrección aquí
         IDCliente.setValue(null);
         TextCliente.clear();
         TextFacilitador.clear();
     }
 
-    // funcion que permite mostrar un dialogo personalizado para el exito de un registro
-    private void mostrarExito(String Message) {
-        Dialog<Void> dialog = new Dialog<Void>(); // se crea un dialogo personalizado
-        dialog.getDialogPane().setPrefHeight(95);  dialog.getDialogPane().setPrefWidth(400);
-        dialog.setTitle("Exito");
-        dialog.setHeaderText(null);
-        //creando al aviso botones personalizados
-        ButtonType ok = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().add(ok);
-        // creando contenido al aviso personalizado
-        Label label = new Label(Message);
-        label.setPrefWidth(150);  // Ancho deseado
-        label.setWrapText(true);  // Permitir envolver el texto si es necesario
-        label.setStyle("-fx-font-size: 14px; -fx-padding: 20px; -fx-text-fill: green; -fx-text-align: center; -fx-font-weight: bold 2px; -fx-font-family: 'Baskerville Old Face', 'Britannic Bold'");
+    private void mostrarExito(String mensaje) {
+        mostrarDialogo("Éxito", mensaje, "-fx-text-fill: green;");
+    }
+
+    private void mostrarError(String mensaje) {
+        mostrarDialogo("Error", mensaje, "-fx-text-fill: red;");
+    }
+
+    private void mostrarDialogo(String titulo, String mensaje, String estiloTexto) {
+        Dialog<Void> dialog = new Dialog<>();
+        dialog.setTitle(titulo);
+        dialog.getDialogPane().setPrefSize(400, 95);
+        ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(okButtonType);
+        Label label = new Label(mensaje);
+        label.setStyle(estiloTexto + " -fx-font-size: 14px; -fx-padding: 20px;");
+        label.setWrapText(true);
         dialog.getDialogPane().setContent(label);
-
         dialog.initStyle(StageStyle.DECORATED);
-
-        Node okNode = dialog.getDialogPane().lookupButton(ok); // se personaliza el boton , Node es una importación que sirve como base para los elementos visuales creados desde programacion
-        if (okNode instanceof  Button) {
-            okNode.setStyle("-fx-pref-width: 217px; -fx-pref-height: 32px");
-            okNode.setStyle("-fx-background-color:  #5F72D9; -fx-text-fill: white; -fx-border-color: transparent; -fx-background-insets: 0"); // se personaliza el color y el texto del boton ok
-            okNode.setOnMouseEntered(e -> {
-                okNode.setStyle("-fx-background-color: #A9D4D9; -fx-text-fill: white");
-            });
-            okNode.setOnMouseExited(e -> {
-                okNode.setStyle("-fx-background-color: #5F72D9; -fx-text-fill: black");
-            });
-        }
-
-
-        // Maneja el tipo de accioo que realizara el boton
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == ok) {
-                return null;
-            }
-            return null;
-        });
         dialog.showAndWait();
     }
 
-
     @FXML
-    private  void HandleMinimizeButton (ActionEvent event) {
-        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    private void HandleMinimizeButton(ActionEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
-
-
 }
