5552a61bd55b443eec464ee9f319539f2a99fcf2
LourdesCabrera1505 <lourdes.90programming@gmail.com>
2024-07-09 15:21:16 -0600

    haciendo arreglos

--- BCN/src/main/java/org/example/bcn/Controller/Compras.java
+++ BCN/src/main/java/org/example/bcn/Controller/Compras.java
@@ -2,14 +2,23 @@ package org.example.bcn.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class Compras {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Compras implements Initializable {
+
 
     @FXML
     private  void HandleMinimizeButton (ActionEvent event) {
         Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
         stage.setIconified(true);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
